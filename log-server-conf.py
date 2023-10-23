15f3cc9adf0981c9dc75b3d60b3a7acbb37e52ea
Akash-Nipunge <119753319+Akash-Nipunge@users.noreply.github.com>
2023-10-23 15:16:49 +0530

    Update log-server-conf.py

--- log-server-conf.py
+++ log-server-conf.py
@@ -1,17 +1,12 @@
-#   create server config file
-#   restart rsyslog
-#   allow TCP
-
 import subprocess
 
-user_ip = "10.0.2.15"
-command=f'''module load=(\"imtcp\")
-input(type=\"imtcp\" port=\"514\")
-$TCPServerAddress {user_ip}'''
+command=f'''module (load=\"imtcp\")
+input(type=\"imtcp\" port=\"514\")'''
 
 try:
     subprocess.run("pkexec touch /etc/rsyslog.d/my.conf", shell=True)
     subprocess.run(f"pkexec echo '{command}' > /etc/rsyslog.d/my.conf", shell=True)
     subprocess.run("pkexec service rsyslog restart", shell=True)
+    subprocess.run("pkexec ufw allow 514/tcp", shell=True)
 except Exception as e:
     print(f"Error: {e}")
