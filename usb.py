f725aa510b107a1c0decee1a13ff51fe2a9df470
Sanved <Sanvedbangale23@gmail.com>
2023-10-23 14:50:39 +0530

    Add files via upload

--- usb.py
+++ usb.py
@@ -2,6 +2,7 @@ import json
 import tkinter as tk
 
 rules_conf_file = '/etc/usbguard/rules.conf'
+rules_conf_file = './sample.txt'
 
 lines_list = []
 
@@ -16,22 +17,46 @@ except Exception as e:
     print(f"An error occurred: {e}")
 
 objects = []
+
 for line in lines_list:
-    temp_list = line.split('"')
-    temp_0 = temp_list[0]
-    temp_0_split = temp_0.split(' ')
-    temp_dict = {}
-    temp_dict["status"] = temp_0_split[0]
-    temp_dict["id"] = temp_0_split[2]
-    temp_dict["serial"] = temp_list[1]
-    temp_dict["name"] = temp_list[3]
-    objects.append(temp_dict)
+    if line != "":
+        temp_list = line.split('"')
+        temp_0 = temp_list[0]
+        temp_0_split = temp_0.split(' ')
+        temp_dict = {}
+        temp_dict["status"] = temp_0_split[0]
+        temp_dict["id"] = temp_0_split[2]
+        temp_dict["serial"] = temp_list[1]
+        temp_dict["name"] = temp_list[3]
+        objects.append(temp_dict)
+
+
+
+def change_rule_status(id_to_block):
+
+    with open(rules_conf_file, 'r') as file:
+        lines = file.readlines()
+
+    for i, line in enumerate(lines):
+        if id_to_block in line:
+            if "allow" in line:
+                lines[i] = line.replace("allow", "block")
+            elif "block" in line:
+                lines[i] = line.replace("block", "allow")
+
+    with open(rules_conf_file, 'w') as file:
+        file.writelines(lines)
+
+
 
 
 def update_status(index, new_status):
     update_display()
+    change_rule_status(objects[index]["id"])
     objects[index]["status"] = new_status
     update_display()
+
+
 
 
 root = tk.Tk()
@@ -66,6 +91,6 @@ def update_display():
 
 update_button = tk.Button(root, text="refresh", command=update_display)
 update_button.pack()
-
+update_display()
 root.mainloop()
 
