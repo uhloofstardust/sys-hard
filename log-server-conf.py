bfa0efe3163c5111210491de12b19866b8937c30
Akash-Nipunge <119753319+Akash-Nipunge@users.noreply.github.com>
2023-10-23 12:40:03 +0530

    Update log-server-conf.py

--- log-server-conf.py
+++ log-server-conf.py
@@ -5,10 +5,13 @@
 import subprocess
 
 user_ip = "10.0.2.15"
+command=f'''module load=(\"imtcp\")
+input(type=\"imtcp\" port=\"514\")
+\$TCPServerAddress {user_ip}'''
 
 try:
     subprocess.run("pkexec touch /etc/rsyslog.d/my.conf", shell=True)
-    subprocess.run(f'pkexec echo -e "module(load=\"imtcp\")\ninput(type=\"imtcp\" port=\"514\")\n\n\$TCPServerAddress {user_ip}" > /etc/rsyslog.d/my.conf', shell=True)
+    subprocess.run(f'pkexec echo '{command}' > /etc/rsyslog.d/my.conf', shell=True)
     subprocess.run("pkexec service rsyslog restart", shell=True)
 except Exception as e:
     print(f"Error: {e}")
