440ec6802c92ed37b4f19da9471ee449085bc916
Akash-Nipunge <119753319+Akash-Nipunge@users.noreply.github.com>
2023-10-25 16:31:02 +0530

    Update rough.py

--- rough.py
+++ rough.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 import os
 from PIL import Image
 import subprocess
+from temp import create_usb_port_management_frame
 
 class App(ctk.CTk):
     def __init__(self):
@@ -34,90 +35,19 @@ class App(ctk.CTk):
         self.appearance_mode_menu = ctk.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"], command=self.change_appearance_mode_event)
         self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
 
+
+
+# home frame
+
+
+##########################################################################
     def create_home_frame(self):
         self.home_frame = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.home_frame.grid_columnconfigure(0, weight=1)
-        self.create_usb_port_management_frame(self.home_frame)
+        create_usb_port_management_frame(self.home_frame,ctk)
 
-    def create_usb_port_management_frame(self, parent_frame):
-        # Your code for USB port management here
-        rules_conf_file = '/etc/usbguard/rules.conf'
-        lines_list = []
-
-        try:
-            with open(rules_conf_file, 'r') as file:
-                for line in file:
-                    lines_list.append(line.strip())
-        except FileNotFoundError:
-            print(f"File not found: {rules_conf_file}")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-
-        objects = []
-
-        for line in lines_list:
-            if line != "":
-                temp_list = line.split('"')
-                temp_0 = temp_list[0]
-                temp_0_split = temp_0.split(' ')
-                temp_dict = {}
-                temp_dict["status"] = temp_0_split[0]
-                temp_dict["id"] = temp_0_split[2]
-                temp_dict["serial"] = temp_list[1]
-                temp_dict["name"] = temp_list[3]
-                objects.append(temp_dict)
-
-        def change_rule_status(id_to_block, new_status):
-            with open(rules_conf_file, 'r') as file:
-                lines = file.readlines()
-
-            for i, line in enumerate(lines):
-                if id_to_block in line:
-                    if "allow" in line:
-                        lines[i] = line.replace("allow", new_status)
-                    elif "block" in line:
-                        lines[i] = line.replace("block", new_status)
-
-            with open(rules_conf_file, 'w') as file:
-                file.writelines(lines)
-            subprocess.run("pkexec service usbguard restart", shell=True)
-
-        def update_status(index, new_status):
-            update_display()
-            change_rule_status(objects[index]["id"], new_status)
-            objects[index]["status"] = new_status
-            update_display()
-
-        title = ctk.CTkLabel(parent_frame, text="USB PORTS MANAGEMENT", font=("Arial", 24), pady=14)
-        title.pack()
-        frame = ctk.CTkFrame(parent_frame)
-        frame.pack()
-
-        circle_labels = []
-
-        for index, obj in enumerate(objects):
-            circle = ctk.CTkLabel(frame, text="●", font=("Arial", 24), width=1, anchor="w")
-            circle.grid(row=index, column=0, padx=10, pady=5)
-            circle_labels.append(circle)
-
-            label = ctk.CTkLabel(frame, font=("Arial", 16), text=f"ID: {obj['id']}, Name: {obj['name']}")
-            label.grid(row=index, column=1, padx=10, pady=8)
-
-            allow_button = ctk.CTkButton(frame, text="Allow", command=lambda i=index: update_status(i, "allow"))
-            allow_button.grid(row=index, column=2, padx=5, pady=8)
-
-            block_button = ctk.CTkButton(frame, text="Block", command=lambda i=index: update_status(i, "block"))
-            block_button.grid(row=index, column=3, padx=5, pady=8)
-
-        def update_display():
-            for i, obj in enumerate(objects):
-                if obj["status"] == "block":
-                    circle_labels[i].configure(text_color="red")
-                else:
-                    circle_labels[i].configure(text_color="green")
-
-        update_display()
-
+#######################################################################
+    
     def create_nav_button(self, parent_frame, text, command):
         return ctk.CTkButton(parent_frame, corner_radius=0, height=40, border_spacing=10, text=text,
                              fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
