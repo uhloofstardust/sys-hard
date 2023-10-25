771c8e6762684e643b9111a08a263eef732bc810
uhloofstardust <sanvedbangale23@gmail.com>
2023-10-25 13:22:14 +0530

    password_policy.py

--- password_policy.py
+++ password_policy.py
@@ -17,6 +17,27 @@ def change_rule_status(filepath, rule, value=0, enable=True):
     
     with open(filepath, 'w') as f:
         f.writelines(newlines)
-        
-def password_policy():
+
+def add_rule(filepath, rule, enable=True):
+    newlines = []
     
+    with open(filepath, 'r') as f:
+        lines = f.readlines()
+    
+    for line in lines:
+        if (line.strip().startswith('# '+rule) or line.strip().startswith(rule)):
+            if enable:
+                newlines.append(rule + '\n')
+            else:
+                newlines.append('# ' + rule + '\n')
+        else:
+            newlines.append(line)
+    
+    with open(filepath, 'w') as f:
+        f.writelines(newlines)
+        
+def password_policy(rule, value, enable):
+    if rule in ['minlen', 'minclass', 'maxrepeat', 'dictcheck', 'usercheck', 'retry']:
+        change_rule_status('/etc/security/pwquality', rule, value, enable)
+    elif rule in ['enforce_for_root']:
+        add_rule('/etc/security/pwquality', rule, enable)
