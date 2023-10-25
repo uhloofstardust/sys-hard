c0c133db86e233adb6f6a7e5f26360da3bf37e13
uhloofstardust <sanvedbangale23@gmail.com>
2023-10-25 16:07:06 +0530

    rough.py

--- rough.py
+++ rough.py
@@ -1,183 +1,69 @@
-import tkinter as tk
 import customtkinter as ctk
-from tkinter import messagebox
+import os
+from PIL import Image
+import subprocess
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Dashboard")
+        self.geometry("700x450")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        self.create_navigation_frame()
+        self.create_home_frame()
+
+    def create_navigation_frame(self):
+        self.navigation_frame = ctk.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(4, weight=1)
+
+        self.navigation_frame_label = ctk.CTkLabel(self.navigation_frame, text="  Ubuntu Hardening", font=ctk.CTkFont(size=15, weight="bold"))
+        self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
+
+        self.home_button = self.create_nav_button(self.navigation_frame, "PortMaster", self.home_button_event)
+        self.frame_2_button = self.create_nav_button(self.navigation_frame, "Frame 2", self.frame_2_button_event)
+        self.frame_3_button = self.create_nav_button(self.navigation_frame, "Frame 3", self.frame_3_button_event)
+
+        self.appearance_mode_menu = ctk.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"], command=self.change_appearance_mode_event)
+        self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
+
+    def create_home_frame(self):
+        self.home_frame = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.home_frame.grid_columnconfigure(0, weight=1)
+        self.create_usb_port_management_frame(self.home_frame)
+
+    def create_usb_port_management_frame(self, parent_frame):
+        title = ctk.CTkLabel(parent_frame, text="USB PORTS MANAGEMENT", font=("Arial", 24), pady=14)
+        title.pack()
+        frame = ctk.CTkFrame(parent_frame)
+        frame.pack()
+
+        # Your code for USB port management here
+        # You can create labels, buttons, and other widgets in this frame
+
+    def create_nav_button(self, parent_frame, text, command):
+        return ctk.CTkButton(parent_frame, corner_radius=0, height=40, border_spacing=10, text=text,
+                             fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                             anchor="w", command=command)
+
+    def home_button_event(self):
+        # Handle the home button event
+        pass
+
+    def frame_2_button_event(self):
+        # Handle the frame 2 button event
+        pass
+
+    def frame_3_button_event(self):
+        # Handle the frame 3 button event
+        pass
+
+    def change_appearance_mode_event(self, new_appearance_mode):
+        ctk.set_appearance_mode(new_appearance_mode)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
 
-
-rules = ['minlen', 'minclass', 'maxrepeat', 'dictcheck', 'usercheck', 'retry']
-
-
-
-
-def change_rule_status(filepath, rule, value=0, enable=True):
-    newlines = []
-    
-    with open(filepath, 'r') as f:
-        lines = f.readlines()
-    
-    for line in lines:
-        if (line.strip().startswith('# '+rule) or line.strip().startswith(rule)):
-            if enable:
-                newlines.append(rule + '=' + str(value) + '\n')
-            else:
-                newlines.append('# ' + rule + '=' + str(value) + '\n')
-        else:
-            newlines.append(line)
-    
-    with open(filepath, 'w') as f:
-        f.writelines(newlines)
-
-
-
-def add_rule(filepath, rule, enable=True):
-    newlines = []
-    
-    with open(filepath, 'r') as f:
-        lines = f.readlines()
-    
-    for line in lines:
-        if (line.strip().startswith('# '+rule) or line.strip().startswith(rule)):
-            if enable:
-                newlines.append(rule + '\n')
-            else:
-                newlines.append('# ' + rule + '\n')
-        else:
-            newlines.append(line)
-    
-    with open(filepath, 'w') as f:
-        f.writelines(newlines)
-
-
-
-def password_policy(rule, value=0, enable=True):
-    if rule in rules:
-        change_rule_status('/etc/security/pwquality.conf', rule, value, enable)
-    elif rule in ['enforce_for_root']:
-        add_rule('/etc/security/pwquality.conf', rule, enable)
-
-root = ctk.CTk()
-root.title("System Configuration")
-
-
-historynumber_var = ctk.CTkEntry(master=root)
-minlen_var = ctk.CTkEntry(master=root)
-minclass_var = ctk.CTkEntry(master=root)
-maxrepeat_var = ctk.CTkEntry(master=root)
-dictcheck_var = ctk.CTkEntry(master=root)
-usercheck_var = ctk.CTkEntry(master=root)
-enforceroot_var = ctk.CTkEntry(master=root)
-retry_var = ctk.CTkEntry(master=root)
-
-
-
-
-# minlen 
-
-minlen_label = ctk.CTkLabel(root, text='minlen')
-minlen_label.grid(row=0, column=0, sticky='w')
-
-minlen_entry = ctk.CTkEntry(master=root, placeholder_text='minlen', width=150, height=25, border_width=2, corner_radius=5)
-minlen_entry.grid(row=0, column=1, sticky='w',padx=20,pady=10)
-
-minlen_enable_button = ctk.CTkButton(root, text="Enable", command=lambda : password_policy('minlen', minlen_entry.get(), True))
-minlen_enable_button.grid(row=0, column=2,padx = 10,pady = 10)
-
-minlen_disable_button = ctk.CTkButton(root, text="Disable", command=lambda : password_policy('minlen', 14, False))
-minlen_disable_button.grid(row=0, column=3,padx = 10,pady = 10)
-
-
-# minclass 
-
-minclass_label = ctk.CTkLabel(root, text='minclass')
-minclass_label.grid(row=1, column=0, sticky='w')
-
-minclass_entry = ctk.CTkEntry(master=root, placeholder_text='minclass', width=150, height=25, border_width=2, corner_radius=5)
-minclass_entry.grid(row=1, column=1, sticky='w',padx=20,pady=10)
-
-minclass_enable_button = ctk.CTkButton(root, text="Enable", command=lambda : password_policy('minclass', minclass_entry.get(), True))
-minclass_enable_button.grid(row=1, column=2,padx = 10,pady = 10)
-
-minclass_disable_button = ctk.CTkButton(root, text="Disable", command=lambda : password_policy('minclass', minclass_entry.get(), False))
-minclass_disable_button.grid(row=1, column=3,padx = 10,pady = 10)
-
-
-# maxrepeat 
-
-maxrepeat_label = ctk.CTkLabel(root, text='maxrepeat')
-maxrepeat_label.grid(row=2, column=0, sticky='w')
-
-maxrepeat_entry = ctk.CTkEntry(master=root, placeholder_text='maxrepeat', width=150, height=25, border_width=2, corner_radius=5)
-maxrepeat_entry.grid(row=2, column=1, sticky='w',padx=20,pady=10)
-
-maxrepeat_enable_button = ctk.CTkButton(root, text="Enable", command=lambda : password_policy('maxrepeat', maxrepeat_entry.get(), True))
-maxrepeat_enable_button.grid(row=2, column=2,padx = 10,pady = 10)
-
-maxrepeat_disable_button = ctk.CTkButton(root, text="Disable", command=lambda : password_policy('maxrepeat', maxrepeat_entry.get(), False))
-maxrepeat_disable_button.grid(row=2, column=3,padx = 10,pady = 10)
-
-
-# dictcheck 
-
-dictcheck_label = ctk.CTkLabel(root, text='dictcheck')
-dictcheck_label.grid(row=3, column=0, sticky='w')
-
-dictcheck_entry = ctk.CTkEntry(master=root, placeholder_text='1 or 0', width=150, height=25, border_width=2, corner_radius=5)
-dictcheck_entry.grid(row=3, column=1, sticky='w',padx=20,pady=10)
-
-dictcheck_enable_button = ctk.CTkButton(root, text="Enable", command=lambda : password_policy('dictcheck', dictcheck_entry.get(), True))
-dictcheck_enable_button.grid(row=3, column=2,padx = 10,pady = 10)
-
-dictcheck_disable_button = ctk.CTkButton(root, text="Disable", command=lambda : password_policy('dictcheck', dictcheck_entry.get(), False))
-dictcheck_disable_button.grid(row=3, column=3,padx = 10,pady = 10)
-
-
-# usercheck 
-
-usercheck_label = ctk.CTkLabel(root, text='usercheck')
-usercheck_label.grid(row=4, column=0, sticky='w')
-
-usercheck_entry = ctk.CTkEntry(master=root, placeholder_text='1 or 0', width=150, height=25, border_width=2, corner_radius=5)
-usercheck_entry.grid(row=4, column=1, sticky='w',padx=20,pady=10)
-
-usercheck_enable_button = ctk.CTkButton(root, text="Enable", command=lambda : password_policy('usercheck', usercheck_entry.get(), True))
-usercheck_enable_button.grid(row=4, column=2,padx = 10,pady = 10)
-
-usercheck_disable_button = ctk.CTkButton(root, text="Disable", command=lambda : password_policy('usercheck', usercheck_entry.get(), False))
-usercheck_disable_button.grid(row=4, column=3,padx = 10,pady = 10)
-
-
-
-# enforce_for_root
-
-enforce_for_root_label = ctk.CTkLabel(root, text='enforce for root')
-enforce_for_root_label.grid(row=5, column=0, sticky='w')
-
-enforce_for_root_entry = ctk.CTkEntry(master=root, placeholder_text='1 or 0', width=150, height=25, border_width=2, corner_radius=5)
-enforce_for_root_entry.grid(row=5, column=1, sticky='w', padx=20, pady=10)
-
-enforce_for_root_enable_button = ctk.CTkButton(root, text="Enable", command=lambda: password_policy('enforce_for_root', enforce_for_root_entry.get(), True))
-enforce_for_root_enable_button.grid(row=5, column=2, padx=10, pady=10)
-
-enforce_for_root_disable_button = ctk.CTkButton(root, text="Disable", command=lambda: password_policy('enforce_for_root', enforce_for_root_entry.get(), False))
-enforce_for_root_disable_button.grid(row=5, column=3, padx=10, pady=10)
-
-
-# retry
-
-retry_label = ctk.CTkLabel(root, text='retry')
-retry_label.grid(row=6, column=0, sticky='w')
-
-retry_entry = ctk.CTkEntry(master=root, placeholder_text='retry', width=150, height=25, border_width=2, corner_radius=5)
-retry_entry.grid(row=6, column=1, sticky='w', padx=20, pady=10)
-
-retry_enable_button = ctk.CTkButton(root, text="Enable", command=lambda: password_policy('retry', retry_entry.get(), True))
-retry_enable_button.grid(row=6, column=2, padx=10, pady=10)
-
-retry_disable_button = ctk.CTkButton(root, text="Disable", command=lambda: password_policy('retry', retry_entry.get(), False))
-retry_disable_button.grid(row=6, column=3, padx=10, pady=10)
-
-
-
-
-
-
-root.mainloop()
