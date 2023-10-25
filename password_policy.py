c49b947818b1f24ff28f4ca0a840c2a6d83511ad
uhloofstardust <sanvedbangale23@gmail.com>
2023-10-25 13:14:53 +0530

    password_policy.py

--- password_policy.py
+++ password_policy.py
@@ -9,11 +9,14 @@ def change_rule_status(filepath, rule, value=0, enable=True):
     for line in lines:
         if (line.strip().startswith('# '+rule) or line.strip().startswith(rule)):
             if enable:
-                newlines.append(rule + '=' + value)
+                newlines.append(rule + '=' + value + '\n')
             else:
-                newlines.append('# ' + rule)
+                newlines.append('# ' + rule + '=' + value + '\n')
         else:
             newlines.append(line)
     
     with open(filepath, 'w') as f:
         f.writelines(newlines)
+        
+def password_policy():
+    
