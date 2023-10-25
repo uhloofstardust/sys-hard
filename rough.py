7525e96c7bba940ae9da2050c9074f8fc60ebf38
uhloofstardust <sanvedbangale23@gmail.com>
2023-10-25 16:14:44 +0530

    rough.py

--- rough.py
+++ rough.py
@@ -27,7 +27,7 @@ class App(ctk.CTk):
         self.frame_3_button = self.create_nav_button(self.navigation_frame, "Frame 3", self.frame_3_button_event)
 
         self.appearance_mode_menu = ctk.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"], command=self.change_appearance_mode_event)
-        self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
+        self.appearance_mode_menu.grid(row=1, column=0, padx=20, pady=20, sticky="s")
 
     def create_home_frame(self):
         self.home_frame = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
