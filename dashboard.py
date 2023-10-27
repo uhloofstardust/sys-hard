55db846567402fcb8aea1ef88976d644d16616e7
Sanved <Sanvedbangale23@gmail.com>
2023-10-27 14:10:33 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,8 @@ from file_permissions import create_file_permissions
 from service_management import create_service_management
 from remote_logging import create_logging
 from firewall import create_firewall_management
+from patch_management import create_patch_management
+
 
 class DashboardApp(ctk.CTk):
     def __init__(self):
@@ -19,6 +21,7 @@ class DashboardApp(ctk.CTk):
         self.frame_3 = None
         self.frame_4 = None
         self.frame_5 = None
+        self.frame_6 = None
 
     def create_navigation_frame(self):
         self.navigation_frame = ctk.CTkFrame(self, corner_radius=0)
@@ -39,12 +42,15 @@ class DashboardApp(ctk.CTk):
             self.navigation_frame, "  Remote Logging  ", self.frame_4_button_clicked)
         self.frame_5_button = self.create_nav_button(
             self.navigation_frame, "  Firewall  ", self.frame_5_button_clicked)
+        self.frame_6_button = self.create_nav_button(
+            self.navigation_frame, "  Patch Management  ", self.frame_6_button_clicked)
 
         self.frame_1_button.grid(row=1, column=0)
         self.frame_2_button.grid(row=2, column=0)
         self.frame_3_button.grid(row=3, column=0)
         self.frame_4_button.grid(row=4, column=0)
         self.frame_5_button.grid(row=5, column=0)
+        self.frame_6_button.grid(row=6, column=0)
 
         self.appearance_mode_menu = ctk.CTkOptionMenu(
             self.navigation_frame, values=["Light", "Dark", "System"], command=self.change_appearance_mode)
@@ -59,6 +65,8 @@ class DashboardApp(ctk.CTk):
             self.frame_4.grid_forget()
         if self.frame_5:
             self.frame_5.grid_forget()
+        if self.frame_6:
+            self.frame_6.grid_forget()
         if self.frame_1:
             self.frame_1.grid(row=0, column=1, sticky="nsew")
         else:
@@ -75,6 +83,8 @@ class DashboardApp(ctk.CTk):
             self.frame_4.grid_forget()
         if self.frame_5:
             self.frame_5.grid_forget()
+        if self.frame_6:
+            self.frame_6.grid_forget()
         if self.frame_2:
             self.frame_2.grid(row=0, column=1, sticky="nsew")
         else:
@@ -91,6 +101,8 @@ class DashboardApp(ctk.CTk):
             self.frame_4.grid_forget()
         if self.frame_5:
             self.frame_5.grid_forget()
+        if self.frame_6:
+            self.frame_6.grid_forget()
         if self.frame_3:
             self.frame_3.grid(row=0, column=1, sticky="nsew")
         else:
@@ -107,6 +119,8 @@ class DashboardApp(ctk.CTk):
             self.frame_3.grid_forget()
         if self.frame_5:
             self.frame_5.grid_forget()
+        if self.frame_6:
+            self.frame_6.grid_forget()
         if self.frame_4:
             self.frame_4.grid(row=0, column=1, sticky="nsew")
         else:
@@ -123,12 +137,32 @@ class DashboardApp(ctk.CTk):
             self.frame_3.grid_forget()
         if self.frame_4:
             self.frame_4.grid_forget()
+        if self.frame_6:
+            self.frame_6.grid_forget()
         if self.frame_5:
             self.frame_5.grid(row=0, column=1, sticky="nsew")
         else:
             self.frame_5 = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
             self.frame_5.grid(row=0, column=1, sticky="nsew")
             create_firewall_management(self.frame_5, ctk)
+
+    def create_frame_6(self):
+        if self.frame_1:
+            self.frame_1.grid_forget()
+        if self.frame_2:
+            self.frame_2.grid_forget()
+        if self.frame_3:
+            self.frame_3.grid_forget()
+        if self.frame_4:
+            self.frame_4.grid_forget()
+        if self.frame_5:
+            self.frame_5.grid_forget()
+        if self.frame_6:
+            self.frame_6.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.frame_6 = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
+            self.frame_6.grid(row=0, column=1, sticky="nsew")
+            create_patch_management(self.frame_6, ctk)
 
     def create_nav_button(self, parent_frame, text, command):
         return ctk.CTkButton(parent_frame, corner_radius=0, height=40, border_spacing=10, text=text, fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), anchor="w", command=command)
@@ -147,6 +181,9 @@ class DashboardApp(ctk.CTk):
 
     def frame_5_button_clicked(self):
         self.create_frame_5()
+
+    def frame_6_button_clicked(self):
+        self.create_frame_6()
 
     def change_appearance_mode(self, new_appearance_mode):
         ctk.set_appearance_mode(new_appearance_mode)
