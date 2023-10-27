f86f07b16a8c8726977b88bb07ec65aacc1d1fda
Sanved <Sanvedbangale23@gmail.com>
2023-10-27 13:34:11 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,12 +3,13 @@ from usb_management import create_usb_port_management_frame
 from file_permissions import create_file_permissions
 from service_management import create_service_management
 from remote_logging import create_logging
+from firewall import create_firewall_management
 
 class DashboardApp(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Dashboard")
-        self.geometry("900x600")
+        self.geometry("1200x600")
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
@@ -17,33 +18,37 @@ class DashboardApp(ctk.CTk):
         self.frame_2 = None
         self.frame_3 = None
         self.frame_4 = None
+        self.frame_5 = None
 
     def create_navigation_frame(self):
         self.navigation_frame = ctk.CTkFrame(self, corner_radius=0)
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
-        self.navigation_frame.grid_rowconfigure(5, weight=1)
+        self.navigation_frame.grid_rowconfigure(12, weight=1)
 
         self.navigation_frame_label = ctk.CTkLabel(
-            self.navigation_frame, text="Ubuntu Hardening", font=ctk.CTkFont(size=15, weight="bold"))
+            self.navigation_frame, text="Ubuntu Hardening", font=ctk.CTkFont(size=15, weight="bold", family="Ubuntu"))
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
 
         self.frame_1_button = self.create_nav_button(
-            self.navigation_frame, "PortMaster", self.frame_1_button_clicked)
+            self.navigation_frame, "  PortMaster  ", self.frame_1_button_clicked)
         self.frame_2_button = self.create_nav_button(
-            self.navigation_frame, "File Permissions", self.frame_2_button_clicked)
+            self.navigation_frame, "  File Permissions  ", self.frame_2_button_clicked)
         self.frame_3_button = self.create_nav_button(
-            self.navigation_frame, "Service Management", self.frame_3_button_clicked)
+            self.navigation_frame, "  Service Management  ", self.frame_3_button_clicked)
         self.frame_4_button = self.create_nav_button(
-            self.navigation_frame, "IP Interface", self.frame_4_button_clicked)
+            self.navigation_frame, "  Remote Logging  ", self.frame_4_button_clicked)
+        self.frame_5_button = self.create_nav_button(
+            self.navigation_frame, "  Firewall  ", self.frame_5_button_clicked)
 
         self.frame_1_button.grid(row=1, column=0)
         self.frame_2_button.grid(row=2, column=0)
         self.frame_3_button.grid(row=3, column=0)
         self.frame_4_button.grid(row=4, column=0)
+        self.frame_5_button.grid(row=5, column=0)
 
         self.appearance_mode_menu = ctk.CTkOptionMenu(
             self.navigation_frame, values=["Light", "Dark", "System"], command=self.change_appearance_mode)
-        self.appearance_mode_menu.grid(row=5, column=0, padx=20, pady=20, sticky="s")
+        self.appearance_mode_menu.grid(row=12, column=0, padx=0, pady=20, ipadx=0,sticky="s")
 
     def create_frame_1(self):
         if self.frame_2:
@@ -52,6 +57,8 @@ class DashboardApp(ctk.CTk):
             self.frame_3.grid_forget()
         if self.frame_4:
             self.frame_4.grid_forget()
+        if self.frame_5:
+            self.frame_5.grid_forget()
         if self.frame_1:
             self.frame_1.grid(row=0, column=1, sticky="nsew")
         else:
@@ -66,6 +73,8 @@ class DashboardApp(ctk.CTk):
             self.frame_3.grid_forget()
         if self.frame_4:
             self.frame_4.grid_forget()
+        if self.frame_5:
+            self.frame_5.grid_forget()
         if self.frame_2:
             self.frame_2.grid(row=0, column=1, sticky="nsew")
         else:
@@ -80,6 +89,8 @@ class DashboardApp(ctk.CTk):
             self.frame_2.grid_forget()
         if self.frame_4:
             self.frame_4.grid_forget()
+        if self.frame_5:
+            self.frame_5.grid_forget()
         if self.frame_3:
             self.frame_3.grid(row=0, column=1, sticky="nsew")
         else:
@@ -94,12 +105,30 @@ class DashboardApp(ctk.CTk):
             self.frame_2.grid_forget()
         if self.frame_3:
             self.frame_3.grid_forget()
+        if self.frame_5:
+            self.frame_5.grid_forget()
         if self.frame_4:
             self.frame_4.grid(row=0, column=1, sticky="nsew")
         else:
             self.frame_4 = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
             self.frame_4.grid(row=0, column=1, sticky="nsew")
             create_logging(self.frame_4, ctk)
+
+    def create_frame_5(self):
+        if self.frame_1:
+            self.frame_1.grid_forget()
+        if self.frame_2:
+            self.frame_2.grid_forget()
+        if self.frame_3:
+            self.frame_3.grid_forget()
+        if self.frame_4:
+            self.frame_4.grid_forget()
+        if self.frame_5:
+            self.frame_5.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.frame_5 = ctk.CTkFrame(self, corner_radius=0, fg_color="transparent")
+            self.frame_5.grid(row=0, column=1, sticky="nsew")
+            create_firewall_management(self.frame_5, ctk)
 
     def create_nav_button(self, parent_frame, text, command):
         return ctk.CTkButton(parent_frame, corner_radius=0, height=40, border_spacing=10, text=text, fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"), anchor="w", command=command)
@@ -115,6 +144,9 @@ class DashboardApp(ctk.CTk):
 
     def frame_4_button_clicked(self):
         self.create_frame_4()
+
+    def frame_5_button_clicked(self):
+        self.create_frame_5()
 
     def change_appearance_mode(self, new_appearance_mode):
         ctk.set_appearance_mode(new_appearance_mode)
